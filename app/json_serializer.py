25d39d697b27049ded07312d9869ea477edcf739
Mihail Komarov <michaelkomarov03@gmail.com>
2025-12-02 22:31:50 +0200

    add json_serializer

---
+++ app/json_serializer.py
@@ -0,0 +1,35 @@
+import model
+from pathlib import Path
+
+def serialize_to_json(job_object: model.VideoEncodingJob, output_path: str | Path):
+    p = Path(output_path)
+
+    p.parent.mkdir(parents=True, exist_ok=True)
+
+    try:
+        json_string = job_object.model_dump_json(indent=4)
+
+        with open(p, "w", encoding="utf-8") as f:
+            f.write(json_string)
+
+        print(f"Json saved successfully: {p.resolve()}")
+
+    except Exception as e:
+        print(f"Error serializing json. Output path: {output_path}. Exception: {e}")
+
+
+def load_from_json(input_path: str | Path) -> model.VideoEncodingJob:
+    p = Path(input_path)
+
+    if not p.is_file():
+        raise FileNotFoundError(f"File not found for serialization: {p.resolve()}")
+
+    try:
+        json_content = p.read_text(encoding="utf-8")
+        job_object = model.VideoEncodingJob.model_validate_json(json_content)
+
+        print(f"Json loaded: {p.resolve()}")
+        return job_object
+
+    except Exception as e:
+        raise ValueError(f"Error loading json file. Input path: {input_path}. Exception: {e}")

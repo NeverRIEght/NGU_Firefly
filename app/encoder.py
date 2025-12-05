ac05eaa850bdb6b02b4517a941c1d759b041af76
Mihail Komarov <michaelkomarov03@gmail.com>
2025-12-05 19:08:13 +0200

    add encoder initial file

---
+++ app/encoder.py
@@ -0,0 +1,13 @@
+def encode_job(job: EncoderJobContext) -> EncoderJobContext:
+    # Extract current stage
+    # if PREPARED - start binary search with initial values from .env
+    # if SEARCHING_CRF - start binary search with the values from the json data
+    # if CRF_FOUND - perform one final encoding with the "crf_range_min" from the json data. Also, perform a check if the "crf_range_min" is the same as the "crf_range_max"
+    return EncoderJobContext()
+
+
+class EncodingStage(Enum):
+    PREPARED = "job_prepared"
+    SEARCHING_CRF = "searching_crf"
+    CRF_FOUND = "perfect_crf_found"
+    COMPLETED = "encoding_completed"

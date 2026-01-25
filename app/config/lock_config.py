add0ef8f3855e789abd42575b3bdc8f62b3aac5c
Mihail Komarov <michaelkomarov03@gmail.com>
2026-01-25 18:15:34 +0200

    change application lock suffix

--- app/config/lock_config.py
+++ app/config/lock_config.py
@@ -1,6 +1,6 @@
 class LockConfig:
     DEFAULT_TIMEOUT = 5.0
-    APPLICATION_LOCK_NAME = ".firefly.lock"
+    APPLICATION_LOCK_NAME = ".firefly"
     JOB_LOCK_PREFIX = ".firefly_job_"
     METADATA_LOCK_SUFFIX = ".lock"
     STALE_LOCK_THRESHOLD = 3600  # 1 hour

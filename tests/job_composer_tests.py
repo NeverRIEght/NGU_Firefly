f071cfb4c5065ad61944ea15976ddfa0b4545af9
Mihail Komarov <michaelkomarov03@gmail.com>
2026-02-05 14:42:10 +0200

    add job_composer_tests/test_creates_directories_for_jobs

--- tests/job_composer_tests.py
+++ tests/job_composer_tests.py
@@ -1,6 +1,15 @@
 from app import job_composer
 
 
+def test_creates_directories_for_jobs(mock_app_config):
+    jobs_dir = mock_app_config.output_dir / "firefly" / "data" / "jobs"
+    assert not jobs_dir.exists()
+
+    job_composer.compose_jobs()
+
+    assert jobs_dir.exists()
+    assert jobs_dir.is_dir()
+
 def test_load_existing_jobs_deletes_invalid_json(mock_app_config):
     jobs_dir = mock_app_config.output_dir / "firefly" / "data" / "jobs"
     jobs_dir.mkdir(parents=True, exist_ok=True)

bc12cef1c56d72e60c65015de3ec80ab5b0b0a20
Mihail Komarov <michaelkomarov03@gmail.com>
2026-08-09 16:57:18 +0300

    feat: add dto.Job link to dto.Segment

--- app/model/dto/segment.py
+++ app/model/dto/segment.py
@@ -2,11 +2,12 @@ from typing import List, Optional
 
 from pydantic import BaseModel, Field
 
-from app.model.dto import SegmentStatus, Iteration
+from app.model.dto import Iteration, Job, SegmentStatus
 
 
 class Segment(BaseModel):
     id: Optional[int] = None
+    job: Optional[Job] = None
     from_frame: Optional[int] = None
     to_frame: Optional[int] = None
     status: Optional[SegmentStatus] = None

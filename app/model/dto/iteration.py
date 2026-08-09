9276a5bcc7ccdd6dbec0ea1a80bea29ce79d74f4
Mihail Komarov <michaelkomarov03@gmail.com>
2026-08-09 16:58:58 +0300

    feat: add dto.Segment link to dto.Iteration

--- app/model/dto/iteration.py
+++ app/model/dto/iteration.py
@@ -2,11 +2,12 @@ from typing import Optional
 
 from pydantic import BaseModel
 
-from app.model.dto import Environment, ExecutionData, IterationStage, Video
+from app.model.dto import Environment, ExecutionData, IterationStage, Segment, Video
 
 
 class Iteration(BaseModel):
     id: Optional[int] = None
+    segment: Optional[Segment] = None
     stage: Optional[IterationStage] = None
     video: Optional[Video] = None
     environment: Optional[Environment] = None

411d16d9912185be794ce0f87884d3e1021e32d6
Mihail Komarov <michaelkomarov03@gmail.com>
2026-09-17 20:08:28 +0300

    refactor: reorder imports in composer.scoring.init

--- app/composer/scoring/__init__.py
+++ app/composer/scoring/__init__.py
@@ -3,7 +3,8 @@ from app.composer.scoring.job_scorer import JobScorer
 from app.composer.scoring.scoring_exception import ScoringException
 
 __all__ = [
+    "AbstractScoringRule",
     "JobScorer",
     "ScoringException",
-    "AbstractScoringRule",
 ]
+
